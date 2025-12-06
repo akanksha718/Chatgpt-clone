315ada30404effe451ecae9eff9ab860fa2982e1
Akanksha negi <ankiakanksha707@gmail.com>
2025-12-06 18:13:53 +0530

    hi

---
+++ client/src/components/Meassage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Meassage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Meassage
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,59 @@
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets";
+import React from 'react'
+
+const AppContext = React.createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = React.useState(null);
+    const [chats, setChats] = React.useState([]);
+    const [selectedChat, setSelectedChat] = React.useState(null);
+    const [theme, setTheme] = React.useState(localStorage.getItem("theme") || "light");
+    const fetchUser = async () => {
+        setUser(dummyUserData);
+    };
+    const fetchUserchat = async () => {
+        setChats(dummyChats);
+        setSelectedChat(dummyChats[0]);
+    };
+    React.useEffect(() => {
+        fetchUser();
+    }, []);
+    React.useEffect(() => {
+        if (user) {
+            fetchUserchat();
+        }
+        else {
+            setChats([]);
+            setSelectedChat(null);
+            navigate("/login");
+        }
+    }, [user]);
+    React.useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.classList.add("dark");
+        }
+        else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [theme]);
+    const value = {
+        navigate,
+        user,
+        setUser,
+        chats,
+        setChats,
+        selectedChat,
+        setSelectedChat,
+        theme,
+        setTheme,
+        fetchUser,
+    };
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+};
+export const useAppContext = () => React.useContext(AppContext);
