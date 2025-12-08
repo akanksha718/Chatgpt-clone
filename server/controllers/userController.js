9b1d6e49ad3606f7ab1706cc115dbb7a285bff16
Akanksha negi <ankiakanksha707@gmail.com>
2025-12-08 10:26:12 +0530

    g

---
+++ server/controllers/userController.js
@@ -0,0 +1,51 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const genersteToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+}
+
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.json({ success: false, message: "User already exists" });
+        }
+        const newUser = await User.create({ name, email, password });
+        const token = genersteToken(newUser._id);
+        res.json({ success: true, token, message: "User registered successfully" });
+    } catch (error) {
+        res.json({ success: false, message: "Server error" });
+    }
+}
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
+        const token = genersteToken(user._id);
+        res.json({ success: true, token, message: "Login successful" });
+    }
+    catch (error) {
+        res.json({ success: false, message: "Server error" });
+    }
+}
+
+export const getUser = async (req, res) => {
+    try {
+        const user = req.user;
+        return res.json({ success: true, user });
+    }
+    catch (error) {
+        return res.json({ success: false, message: "Server error" });
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,2 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/db.js';
+
+const app = express();
+
+await connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
